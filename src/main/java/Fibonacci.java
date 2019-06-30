0f4937d176619abd38e75cd8e5ce7f01ec6fc35b
Journey <brit0104@hotmail.com>
2019-06-30 15:30:51 +0900

    [algorithms] fibonacci - score 90

---
+++ src/main/java/Fibonacci.java
@@ -0,0 +1,22 @@
+/**
+ * @author jiyouneo
+ * @since 2019. 06. 30
+ */
+public class Fibonacci {
+	public int cal(int num) {
+		int start = 0;
+		int next = 1;
+
+		int cal = 0;
+
+		for (int i = 0; i < num - 1 ; i++) {
+			cal = start + next;
+
+			start = next;
+			next = cal;
+
+		}
+
+		return cal;
+	}
+}
---
+++ src/test/java/FibonacciTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jiyouneo
+ * @since 2019. 06. 30
+ */
+public class FibonacciTest {
+
+	@Test
+	public void fibonacci_when10() {
+		Fibonacci fibonacci = new Fibonacci();
+
+		assertEquals(55, fibonacci.cal(10));
+	}
+
+	@Test
+	public void fibonacci_wheno0() {
+		Fibonacci fibonacci = new Fibonacci();
+
+		assertEquals(0, fibonacci.cal(0));
+	}
+
+}
